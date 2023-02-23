89245fc34a7c95fb90258289cb03b72475012871
肖聪 <3156615583@qq.com>
2023-02-24 04:29:37 +0800

    增加logback配置文件

--- src/main/java/com/Baicai/baicai/BaicaiApplication.java
+++ src/main/java/com/Baicai/baicai/BaicaiApplication.java
@@ -1,7 +1,9 @@
 package com.Baicai.baicai;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class BaicaiApplication {
