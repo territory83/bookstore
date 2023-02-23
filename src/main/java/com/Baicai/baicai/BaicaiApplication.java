168897d6921b23d4c54ff1bfd576fbdc3edf8405
肖聪 <3156615583@qq.com>
2023-02-24 03:27:47 +0800

    项目创建

---
+++ src/main/java/com/Baicai/baicai/BaicaiApplication.java
@@ -0,0 +1,13 @@
+package com.Baicai.baicai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaicaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaicaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Baicai/baicai/BaicaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Baicai.baicai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaicaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
