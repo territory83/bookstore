715db1704b3ac42d170da59fb23cc535372ddc13
肖聪 <3156615583@qq.com>
2023-02-24 05:44:08 +0800

    四个配置文件

--- src/main/java/com/Baicai/baicai/controller/TestController.java
+++ src/main/java/com/Baicai/baicai/controller/TestController.java
@@ -2,6 +2,7 @@ package com.Baicai.baicai.controller;
 
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,11 @@ public class TestController {
      * @return
      */
 
+    @Value("${test.hello}")
+    private String testhello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello Baicai";
+        return "Hello Baicai" + testhello;
     }
 }
