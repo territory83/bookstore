5bf15000af3b2c839a630499ed8b673ead08121a
肖聪 <3156615583@qq.com>
2023-02-24 04:46:15 +0800

    修改了启动信息

--- src/main/java/com/Baicai/baicai/BaicaiApplication.java
+++ src/main/java/com/Baicai/baicai/BaicaiApplication.java
@@ -1,15 +1,23 @@
 package com.Baicai.baicai;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
 public class BaicaiApplication {
 
+	public static final Logger LOG =  LoggerFactory.getLogger(BaicaiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(BaicaiApplication.class, args);
+		SpringApplication app = new SpringApplication(BaicaiApplication.class);
+		Environment env =app.run(args).getEnvironment();
+		LOG.info("启动成功");
+		LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
 	}
 
 }
